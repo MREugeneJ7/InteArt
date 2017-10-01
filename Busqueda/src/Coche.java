3da622247d92c97f7bc2dc6e98b7e93b12bb656c
MREugeneJ7 <32437774+MREugeneJ7@users.noreply.github.com>
2017-10-01 19:42:53 +0100

    Par de definiciones

--- Busqueda/src/Coche.java
+++ Busqueda/src/Coche.java
@@ -1,4 +1,10 @@
 
 public class Coche {
-
+		boolean Sensores[];
+		
+		
+		public void Coche()
+		{
+			Sensores = new boolean[4];
+		}
 }
--- Busqueda/src/Entorno.java
+++ Busqueda/src/Entorno.java
@@ -1,4 +1,10 @@
 
 public class Entorno {
 
-}
+	Object matriz[][];
+	
+	public void Entorno(int N, int M)
+	{
+		matriz = new Object[N][M]; 
+	}
+};
