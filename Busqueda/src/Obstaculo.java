133cfd110872e1a452f6ed631b13f6b8788fac12
alu0101000756 <alu0101000756@cc1232.etsii.ull.es>
2017-10-02 15:52:59 +0100

    Unido con silver haha saludos

--- Busqueda/src/Obstaculo.java
+++
@@ -1,8 +0,0 @@
-
-public class Obstaculo extends Miembros {
-
-	public Obstaculo()
-	{
-		name='o';
-	}
-}
--- src/Entorno.java
+++ src/Entorno.java
@@ -76,14 +76,52 @@ public class Entorno {
 		}
 	}
 	
+	public static Entorno Menu() {
+		boolean salir = false;
+		int opcion,n,m;
+		Entorno prueba = new Entorno();
+		
+		while (!salir) {
+            System.out.println("1. Constructor aleatorio");
+            System.out.println("2. Constructor manual");
+            System.out.println("3. Cambiar porcentaje");
+            System.out.println("4. Salir");
+            try {
+            	 
+                System.out.println("Escribe una de las opciones");
+                opcion = in.nextInt();
+ 
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Inserte N");
+                        n = in.nextInt();
+                        System.out.println("Inserte M");
+                        m = in.nextInt();
+                        prueba = new Entorno(n,m);
+                        break;
+                    case 2:
+                        System.out.println("Has seleccionado la opcion 2");
+                        break;
+                    case 3:
+                        System.out.println("Has seleccionado la opcion 3");
+                        break;
+                    case 4:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Solo números entre 1 y 4");
+                }
+            } catch (InputMismatchException e) {
+                    System.out.println("Debes insertar un número");
+                    in.next();
+                }
+		}
+		return prueba;
+	}
+	
 	public static void main(String args[])
 	{
-		System.out.println("Inserte N");
-		in = new Scanner(System.in);
-		int N = in.nextInt();
-		System.out.println("Inserte M");
-		int M = in.nextInt();
-		Entorno prueba = new Entorno(N,M);
+		Entorno prueba = Entorno.Menu();
 		prueba.show();
 		System.exit(0);
 	}
