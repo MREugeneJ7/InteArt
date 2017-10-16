4de9db3e5e83cffc43a38345a624d50cc84d8dcd
alu0100974331 <alu0100974331@cc1334.etsii.ull.es>
2017-10-16 16:05:49 +0100

    Meta conociendo sus corrdenadas y compare de cercanía con meta.

--- src/Meta.java
+++ src/Meta.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+
 /**
  * IA Pract1
  * Meta.java
@@ -6,9 +8,22 @@
  * @author G.P.A (Gruppies de Pablo Alboran)
  * @version 1.1.a 9/10/2017
  */
-public class Meta extends Miembros {
+public class Meta extends Miembros implements Comparator<Coordenada> {
 	/** Constructor de la clase Meta*/
-	public Meta() {
+	private Coordenada miPos;
+	public Meta(Coordenada pos) {
 		name = 'M';
+		miPos = pos;
+	}
+
+	@Override
+	/**
+	 * Si la primera es más cercana a la meta devolverá 
+	 * un negativo en caso contrario un positivo
+	 */
+	public int compare(Coordenada o1, Coordenada o2) {
+		o1 = o1.diff(miPos);
+		o2 = o2.diff(miPos);		
+		return (Math.abs(o1.getX())+Math.abs(o1.getY()))-(Math.abs(o2.getX())+Math.abs(o2.getY()));
 	}
 }
