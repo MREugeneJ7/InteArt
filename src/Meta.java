ce7af68cf199c9de96326260152aa340c35e57e1
alu0100904932 <alu0100904932@cc1312.etsii.ull.es>
2017-10-30 15:45:51 +0000

    Algoritmos de distancia y resultados de las pruebas.

--- src/Meta.java
+++ src/Meta.java
@@ -24,6 +24,14 @@ public class Meta extends Miembros implements Comparator<Coordenada> {
 	public int compare(Coordenada o1, Coordenada o2) {
 		o1 = o1.diff(miPos);
 		o2 = o2.diff(miPos);		
+		/**
+		 * Manhattan
 		return (Math.abs(o1.getX()) + Math.abs(o1.getY())) - (Math.abs(o2.getX()) + Math.abs(o2.getY()));
+		*/
+		/**
+		 * Euclídea
+		return (int) Math.signum(Math.sqrt(Math.pow(o1.getX(), 2) + Math.pow(o1.getY(), 2)) - Math.sqrt(Math.pow(o2.getX(), 2) + Math.pow(o2.getY(), 2)));
+		*/
+		return Math.max(Math.abs(o1.getX()), Math.abs(o1.getY())) - Math.max(Math.abs(o2.getX()), Math.abs(o2.getY()));
 	}
 }
